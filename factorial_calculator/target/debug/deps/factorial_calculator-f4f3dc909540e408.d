b2fa3bf8fe9baeab90a2d95d1f811cc11637a441
Abah Dominic <constantx003@gmail.com>
2025-06-26 06:14:03 +0100

    build factorial calc

---
+++ factorial_calculator/target/debug/deps/factorial_calculator-f4f3dc909540e408.d
@@ -0,0 +1,5 @@
+/Users/macbookpro/rusthello/factorial_calculator/target/debug/deps/factorial_calculator-f4f3dc909540e408: src/main.rs
+
+/Users/macbookpro/rusthello/factorial_calculator/target/debug/deps/factorial_calculator-f4f3dc909540e408.d: src/main.rs
+
+src/main.rs:
---
+++ factorial_calculator/target/debug/factorial_calculator.d
@@ -0,0 +1,1 @@
+/Users/macbookpro/rusthello/factorial_calculator/target/debug/factorial_calculator: /Users/macbookpro/rusthello/factorial_calculator/src/main.rs
